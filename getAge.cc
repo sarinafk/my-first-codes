4a6eecfeb8e6b67e96dea08a041a3900f708e89a
Sarina Fakour <sarinaf2013@gmail.com>
2022-12-20 22:28:22 +0330

    Code for numbers sum and age preview added.

---
+++ getAge.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int age;
+    std::cout << "Hi! Please enter your age: ";
+    std::cin >> age;
+
+    std::cout << "You are " << age << " years old!\n";
+    return 0;
+}
