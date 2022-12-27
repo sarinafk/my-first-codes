756e9648080676321cf4344d06ea673e207dc510
Sarina Fakour <sarinaf2013@gmail.com>
2022-12-27 14:19:10 +0330

    hello world and print input and add two numbers added

---
+++ 1. Introduction/2. Print Input/main.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+int input;
+cout << "please enter a number";
+cin >> input;
+cout << input;
+return 0;
+}
---
+++ 1. Introduction/3. Add Two Numbers/main.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number1;
+    int number2;
+    int sum;
+    cout << "please enter two numbers";
+    cin >> number1;
+    cin >> number2;
+    sum = number1 + number2;
+    cout << sum;
+}
